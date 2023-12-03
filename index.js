064aceaa13aa8ef0b67c4c362fe42000558ff219
apanacode <sammengi.sailokesh@gmail.com>
2023-12-03 12:25:37 +0530

    static properites implementation

---
+++ index.js
@@ -0,0 +1,9 @@
+document.writeln("<h3>STATIC PROPERTIES</h3>");
+document.writeln("<br>");
+document.writeln("EULERS NUMBER : ",Math.E,"<br>");
+document.writeln("PI NUMBER : ",Math.PI,"<br>");
+document.writeln("Natural logarithm of 2 : ",Math.LN2,"<br>");
+document.writeln("base 10 logarithm of E : ",Math.LOG10E,"<BR>");
+document.writeln("base 2 logarithm of E",Math.LOG2E,"<BR>");
+document.writeln("square root of 1/2 : ",Math.SQRT1_2,"<BR>");
+document.writeln("Square root of 2 ",Math.SQRT2,"<br>");
